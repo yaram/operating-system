3581e3ef9e8c8d9829dde1788994813c3ed0002a
Wayde Reitsma <pootisspencerherenao@gmail.com>
2021-01-26 23:33:05 +1100

    Add gdb option to run.py

--- run.py
+++ run.py
@@ -8,8 +8,19 @@ import shutil
 def run_command(executable, *arguments):
     subprocess.call([executable, *arguments], stdout=sys.stdout, stderr=sys.stderr)
 
+gdb = False
+
+for argument in sys.argv[1:]:
+    if argument == 'gdb':
+        gdb = True
+
 parent_directory = os.path.dirname(os.path.realpath(__file__))
 
 build_directory = os.path.join(parent_directory, 'build')
 
-run_command(shutil.which('qemu-system-x86_64'), '-monitor', 'stdio', '-kernel', os.path.join(build_directory, 'kernel32.elf'))
+run_command(
+    shutil.which('qemu-system-x86_64'),
+    *(['-S', '-s'] if gdb else []),
+    '-monitor', 'stdio',
+    '-kernel', os.path.join(build_directory, 'kernel32.elf')
+)
