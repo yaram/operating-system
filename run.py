c4767722f26139492b7ebf4f23c5bcb459705a3e
Wayde Reitsma <pootisspencerherenao@gmail.com>
2021-02-27 08:44:31 +1100

    Forward run script options to qemu

--- run.py
+++ run.py
@@ -8,22 +8,16 @@ import shutil
 def run_command(executable, *arguments):
     subprocess.call([executable, *arguments], stdout=sys.stdout, stderr=sys.stderr)
 
-gdb = False
-
-for argument in sys.argv[1:]:
-    if argument == 'gdb':
-        gdb = True
-
 parent_directory = os.path.dirname(os.path.realpath(__file__))
 
 build_directory = os.path.join(parent_directory, 'build')
 
 run_command(
     shutil.which('qemu-system-x86_64'),
-    *(['-S', '-s'] if gdb else []),
     '-monitor', 'stdio',
     '-machine', 'q35',
     '-m', '4G',
-    "-vga", 'virtio',
-    '-kernel', os.path.join(build_directory, 'kernel32.elf')
+    '-vga', 'virtio',
+    '-kernel', os.path.join(build_directory, 'kernel32.elf'),
+    *sys.argv[1:]
 )
