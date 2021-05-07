07c81ff498c050f1db637c09a21d35da25e685ab
Wayde Reitsma <pootisspencerherenao@gmail.com>
2021-05-07 16:01:11 +1000

    Remove commented code

--- src/kernel64/process.h
+++ src/kernel64/process.h
@@ -5,44 +5,6 @@
 #include "bucket_array.h"
 #include "array.h"
 
-/*
-// Positions of members in this struct are VERY IMPORTANT and relied on by assembly code
-struct __attribute__((aligned(16))) GeneralStackFrame {
-    // Base integer registers
-    uint64_t rax;
-    uint64_t rbx;
-    uint64_t rcx;
-    uint64_t rdx;
-    uint64_t rsi;
-    uint64_t rdi;
-    uint64_t r8;
-    uint64_t r9;
-    uint64_t r10;
-    uint64_t r11;
-    uint64_t r12;
-    uint64_t r13;
-    uint64_t r14;
-    uint64_t r15;
-    uint64_t rbp;
-
-    uint8_t padding[8];
-
-    // x87/MMX/SSE registers (must be 16-byte aligned, hence the padding above)
-    uint8_t x87_flags[24];
-    uint32_t mxcsr;
-    uint32_t mxcsr_mask;
-    uint8_t mmx_x87[8][16];
-    uint8_t sse[16][16];
-    uint8_t reserved[96];
-};
-
-struct ProcessStackFrame {
-    GeneralStackFrame general_frame;
-
-    InterruptStackFrame interrupt_frame;
-};
-*/
-
 // Positions of members in this struct are VERY IMPORTANT and relied on by assembly code and the architecture
 struct __attribute__((aligned(16))) ProcessStackFrame {
     // Base integer registers
